ad453dfb61a5e1cd466038d2590610f6b8faf289
Alsandre <86976625+Alsandre@users.noreply.github.com>
2024-12-14 16:48:10 +0400

    chore: implement translation logic

--- src/app/components/WelcomeMessage.tsx
+++ src/app/components/WelcomeMessage.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const WelcomeMessage = () => {
+    const [t] = useTranslation();
     return (
         <div className="ml-[4%]">
             <h1 className="text-[#15593A] dark:text-white font-bold text-[34px] mb-[3%] transition-colors">
-                გამარჯობა!
+                {t("welcome")}
             </h1>
             <p className="w-[91%] h-[10.5%] text-[#15593A] dark:text-white font-light text-[19px] transition-colors">
-                აღმოაჩინე თავისუფალი საპარკინგე ადგილები მარტივად და სწრაფად.
+                {`${t("easily and quickly find free parking places")}.`}
             </p>
         </div>
     );
