ceee396825abd5bbfee23c67812052d75587613f
Alsandre <86976625+Alsandre@users.noreply.github.com>
2024-12-14 17:03:45 +0400

    refactor: logo and installpopup components too meet design requirements

--- src/app/components/InstallPopup.tsx
+++ src/app/components/InstallPopup.tsx
@@ -22,20 +22,15 @@ const InstallPopup: React.FC<TProps> = ({ onClick }) => {
                 onClick={hide}
                 className="w-screen h-screen absolute top-0 bottom-0 flex justify-center items-start z-50"
             >
-                <div className="flex justify-center items-center gap-[30%] w-[70%] h-[8.56%] rounded-[12px] bg-[#F7F7F7] absolute top-[4%] left-[14.5%]  p-[6.4%] border-box">
-                    <Logo />
-                    <div>
-                        <p className="w-[38%] h-[19%] font-bold text-[10px] mb-[15%] text-[#000000]">
-                            დააყენე “სახელი”
-                        </p>
+                <div className="flex justify-center items-center gap-[5%] w-[70%] h-[73px] rounded-[12px] bg-[#F7F7F7] absolute top-[4%] left-[14.5%]  py-[6.4%] px-[3.5%] border-box">
+                    <Logo className="w-[34px] h-[34px] text-[9px]" />
+                    <div className="flex-1">
+                        <p className="font-bold text-[10px] text-[#000000]">დააყენე “სახელი”</p>
                         <p className="w-[16.48%] h-[16.4%] font-light text-[10px]text-[#888888]">
                             sakheli.ge
                         </p>
                     </div>
-                    <button
-                        className=" w-[12.5%] h-[19%] font-bold text-[12px] text-[#567DF4]"
-                        onClick={onClick}
-                    >
+                    <button className="font-bold text-[12px] text-[#567DF4]" onClick={onClick}>
                         Install
                     </button>
                 </div>
--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -5,7 +5,7 @@ interface LogoProps {
 const Logo = ({ className }: LogoProps) => {
     return (
         <div
-            className={`${className ?? ""} w-[36%] h-[47px] bg-[#D9D9D9] flex items-center justify-center ml-[4%] mt-[2.5%]`}
+            className={`${className ?? ""} w-[36%] h-[47px] bg-[#D9D9D9] flex items-center justify-center`}
         >
             <div className="w-[77.5%] h-[63%] flex items-center justify-center p-[11px] ">Logo</div>
         </div>
